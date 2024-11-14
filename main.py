d9c8ecfbfb09b135152ea337974799a08eb8f0ef
MaiQuoclong <162609576+MaiQuoclong@users.noreply.github.com>
2024-11-14 15:50:53 +0700

    main.py

--- main.py
+++ main.py
@@ -10,19 +10,24 @@ with open("./questions.json", "r") as f:
 
 valid_levels = ["easy", "medium", "hard"]
 
+# Route gốc để tránh lỗi 404
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
 @app.get("/api")
-def api(level : Optional[str] = None, limit : Optional[int] = None):
+def api(level: Optional[str] = None, limit: Optional[int] = None):
     if not level:
         return questions
 
     if level.lower() not in valid_levels:
-        return { "Error": "Enter a valid level!", "Valid Levels": ["easy", "medium", "hard"]}
+        return {"Error": "Enter a valid level!", "Valid Levels": ["easy", "medium", "hard"]}
 
     filter_method = lambda list: list["level"] == level.upper()
     return list(filter(filter_method, questions))[0 : limit]
 
 @app.get("/api/random")
-def random_question(level : Optional[str] = None):
+def random_question(level: Optional[str] = None):
     while True:
         question = randomquestion()
 
@@ -33,5 +38,4 @@ def random_question(level : Optional[str] = None):
             if question["level"] == level.upper():
                 return question
         else:
-            return { "Error": "Enter a valid level!", "Valid Levels": ["easy", "medium", "hard"]}
-        
+            return {"Error": "Enter a valid level!", "Valid Levels": ["easy", "medium", "hard"]}
